a1683417b4b88c1526cf5f5e04e7b6eba310f372
Alex Bazlinton <abazlinton@gmail.com>
2019-09-10 16:15:11 +0100

    simplified

--- src/app.js
+++ src/app.js
@@ -5,10 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     el: "#app",
     data: {
       countries: [],
-      selectedCountry: {
-        neighbours: []
-      },
-      favouriteCountries: [],
+      selectedCountry: null,
       worldPopulation: 0,
       selectedCountryIndex: 0
     },
@@ -26,20 +23,11 @@ document.addEventListener("DOMContentLoaded", () => {
       },
       updateSelectedCountry: function(){
         this.selectedCountry = this.countries[this.selectedCountryIndex]
-        this.setNeighbouringCountries()
-      },
-      addToFavourites: function(){
-        this.favouriteCountries.push(this.selectedCountry)
       },
       calculateWorldPopulation: function(){
         this.worldPopulation = this.countries.reduce((runningTotal, country) => {
           return runningTotal + country.population
         }, 0)
-      },
-      setNeighbouringCountries: function(){
-        this.selectedCountry.neighbours = this.countries.filter((country) => {
-          return this.selectedCountry.borders.includes(country.alpha3Code);
-        });
       }
     }
   })
